71ca10bc25b6f654ee2e7b9f0a4c25410bff6fae
Akaolisa Iloezumma <akaolisabenjamin@gmail.com>
2024-07-17 21:10:49 +0100

    new changes

--- Afri_Learn.js
+++ Afri_Learn.js
@@ -1,4 +1,4 @@
-document.getElementById('menu-icon').addEventListener('click', function () {
+document.getElementById('menu-icon').addEventListener('click', function () { 
     const nav = document.getElementById('nav-links');
     nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
 });
